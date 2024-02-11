566eaf5b3d6e083b4a0656f5435fabd7c706e921
Mack <yandow.mackenzie@gmail.com>
2024-02-11 13:42:11 -0700

    did some things with style and structure

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,27 +19,34 @@ export default function nav() {
       $submenu.addClass('hide');
   }
 
+  // Convert to map function for dropdowns
+  // const navbar_maps = [
+  //   [ {title: 'Skills'}, {title:'React'}, {title:'Python'}, {title:'PHP'}, {title:'Javascript'}, {title:'Bootstrap'}, {title:'HTML'}, {title:'Scss/Css'}, {title:'Smarty'}],
+  //   [ {title:'Projects'}, {title:'My Project 1'}, {title:'My Project 2'}, {title:'My Project 3'}, {title:'My Project 4'}, {title:'My Project 5'}],
+  //   [ {title: 'AWS Cert\'s'}, {title:'Cernt Name 1'}, {title:'Cernt Name 2'}, {title:'Cernt Name 3'}, {title:'Cernt Name 4'}, {title:'Cernt Name 5'},]
+  // ];
+
   return (
     <div id="navbar">
       <span className="previous-parent"> <Link to="/"> <FontAwesomeIcon className='previous' icon={faArrowRightToBracket} /> </Link> </span>
       <span className="dropdowns-parent">
 
       <div className="menu" onMouseEnter={toggle_submenu} onMouseLeave={toggle_submenu}>
-          <span className="title"> Skills </span>
-          <ul className="submenu submenu-horizontal hide" >
-            <li className="submenu-link" > <span className="" > React </span> </li>
-            <li className="submenu-link" > <span className="" > Python </span> </li>
-            <li className="submenu-link" > <span className="" > PHP </span> </li>
-            <li className="submenu-link" > <span className="" > Javascript </span> </li>
-            <li className="submenu-link" > <span className="" > Bootstrap </span> </li>
-            <li className="submenu-link" > <span className="" > HTML </span> </li>
-            <li className="submenu-link" > <span className="" > Scss/Css </span> </li>
-            <li className="submenu-link" > <span className="" > Smarty </span> </li>
-          </ul>
-        </div>
+        <span className="title"> Skills </span>
+        <ul className="submenu submenu-horizontal hide" >
+          <li className="submenu-link" > <span> React </span> </li>
+          <li className="submenu-link" > <span> Python </span> </li>
+          <li className="submenu-link" > <span> PHP </span> </li>
+          <li className="submenu-link" > <span> Javascript </span> </li>
+          <li className="submenu-link" > <span> Bootstrap </span> </li>
+          <li className="submenu-link" > <span> HTML </span> </li>
+          <li className="submenu-link" > <span> Scss/Css </span> </li>
+          <li className="submenu-link" > <span> Smarty </span> </li>
+        </ul>
+      </div>
 
         <div className="menu" onMouseEnter={toggle_submenu} onMouseLeave={toggle_submenu}>
-          <Link className="title" to="/projects" > Projects </Link>
+          <span className="title" to="/projects" > Projects </span>
           <ul className="submenu hide" >
             <li className="submenu-link" > <span className="" > My Project 1 </span> </li>
             <li className="submenu-link" > <span className="" > My Project 2 </span> </li>
@@ -50,7 +57,7 @@ export default function nav() {
         </div>
 
         <div className="menu" onMouseEnter={toggle_submenu} onMouseLeave={toggle_submenu}>
-          <Link className="link" to="/"> AWS Cert's </Link>
+          <span className="link" to="/"> AWS Cert's </span>
           <ul className="submenu hide" >
               <li className="submenu-link" > <span className="" > Cernt Name 1 </span> </li>
               <li className="submenu-link" > <span className="" > Cernt Name 2 </span> </li>
