6e495ee990e3ef798d579350c359f831e1462feb
MacKenzie Yandow <mackyandow@MacKenzies-MacBook-Pro.local>
2024-02-10 18:01:21 -0700

    initialized project

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import About from './About';
+import Content from './Content';
+
+
+export default function Body() {
+  return (
+    <>
+        <About />
+        <Content />
+    </>
+  )
+}
