530a721167f44ff320cde11d8a3063d06f135225
Mack <yandow.mackenzie@gmail.com>
2024-02-11 11:33:43 -0700

    updates and buildout for hover effect on navbar submenu sections

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import $ from 'jquery';
+import { Routes, Route, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faCss3, faGit, faGithub, faHtml5, faJs, faLinkedin, faMdb, faNode, faReact } from "@fortawesome/free-brands-svg-icons"
-// import { faFileDownload, faUser, faAddressCard, faBriefcase } from "@fortawesome/free-solid-svg-icons";
-
-// const house = <FontAwesomeIcon icon={faGit} />
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 export default function nav() {
+  const toggle_submenu = (e) => {
+    const $this = $(e.target);
+    const $menu = $this.parent('.menu');
+    var $submenu = $menu.children('.submenu');
+    if($this.hasClass('submenu-link'))
+      $submenu = $this.parent('.submenu');
+
+    if($submenu.length > 0 && $submenu.hasClass('hide'))
+        $submenu.removeClass('hide');
+    else
+      $submenu.addClass('hide');
+  }
+  
   return (
-    <>
-      <div id="navbar">
-        Nabvbar
-          <Link to="/">Return</Link>
-      </div>
-    </>
+    <div id="navbar">
+      <span className="previous-parent"> <Link to="/"> <FontAwesomeIcon className='previous' icon={faArrowRightToBracket} /> </Link> </span>
+      <span className="dropdowns-parent">
+        <Link className="link" to="/"> Contact </Link>
+        <div className="menu" onMouseEnter={toggle_submenu} onMouseLeave={toggle_submenu}>
+          <Link className="title" to="/projects" > Projects </Link>
+          <ul className="submenu hide" >
+            <li className="submenu-link" > <span className="" >My Project 1 </span> </li>
+            <li className="submenu-link" > <span className="" > My Project 2 </span> </li>
+            <li className="submenu-link" > <span className="" > My Project 3 </span> </li>
+            <li className="submenu-link" > <span className="" > My Project 4 </span> </li>
+            <li className="submenu-link" > <span className="" > My Project 5 </span> </li>
+          </ul>
+        </div>
+        <Link className="link" to="/"> AWS Cert's </Link>
+      </span>
+    </div>
   )
 }
